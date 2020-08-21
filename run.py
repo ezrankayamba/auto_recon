3dd8962d37ee2422b5b73c991abeffd4c1896909
ezrankayamba <ezrankayamba@gmail.com>
2020-08-21 16:12:30 +0300

    Read csv pandas

--- run.py
+++ run.py
@@ -11,9 +11,12 @@ with open('credentials.json') as creds_file:
     print(creds)
     with pysftp.Connection(**creds) as sfpt:
         print('Connected...')
-        f_name = f'tigo_disbursement_rpt_21082020_065354_001.csv'
-        r_file = f'airtelmoney/AIRTEL/{f_name}'
-        with sfpt.open(r_file) as csv_file:
-            print(csv_file)
-            data = pd.read_csv(csv_file)
-            print(data.head())
+        f1 = f'W2B_15082020.csv'
+        f2 = f'TIGO_TTCL_2020-08-20.csv'
+        r_f1 = f't-pesa/TIGO/Data/Exchangefiles/{f1}'
+        r_f1 = f't-pesa/TTCL/{f2}'
+        with sfpt.open(r_f1) as csv_file2, sfpt.open(r_f2) as csv_file2:
+            data1 = pd.read_csv(csv_file1)
+            data2 = pd.read_csv(csv_file2)
+            print(data1.head())
+            print(data2.head())
