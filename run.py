e9ee8394cfcd58765d89e8e0814dd821188b2c03
ezrankayamba <ezrankayamba@gmail.com>
2020-08-21 16:13:17 +0300

    Read csv pandas

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ with open('credentials.json') as creds_file:
         f1 = f'W2B_15082020.csv'
         f2 = f'TIGO_TTCL_2020-08-20.csv'
         r_f1 = f't-pesa/TIGO/Data/Exchangefiles/{f1}'
-        r_f1 = f't-pesa/TTCL/{f2}'
+        r_f2 = f't-pesa/TTCL/{f2}'
         with sfpt.open(r_f1) as csv_file2, sfpt.open(r_f2) as csv_file2:
             data1 = pd.read_csv(csv_file1)
             data2 = pd.read_csv(csv_file2)
