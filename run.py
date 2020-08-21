ea9815eb53b371f308c414acca41f8c6650301ad
ezrankayamba <ezrankayamba@gmail.com>
2020-08-21 15:03:27 +0300

    Fix hostkey

--- run.py
+++ run.py
@@ -3,6 +3,9 @@ import pysftp
 
 with open('credentials.json') as creds_file:
     creds = json.load(creds_file)
+    cnopts = pysftp.CnOpts()
+    cnopts.hostkeys = None
+    creds['cnopts'] = cnopts
     print(creds)
     with pysftp.Connection(**creds) as sfpt:
         print('Connected...')
