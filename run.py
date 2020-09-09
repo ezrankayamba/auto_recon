4ff7fb5707d116991fe5c1e90f86c0b8b4d0876c
ezrankayamba <ezrankayamba@gmail.com>
2020-09-09 15:42:59 +0300

    Added explicit day

--- run.py
+++ run.py
@@ -20,8 +20,8 @@ with open('credentials.json') as creds_file:
                 with sfpt.open(r_f1) as csv_file1, sfpt.open(r_f2) as csv_file2:
                     df1 = pd.read_csv(csv_file1)
                     df2 = pd.read_csv(csv_file2)
-                    print(df1.head())
-                    print(df2.head())
+                    # print(df1.head())
+                    # print(df2.head())
                     df2.rename(columns={'Ext_TransID': 'TRANSFER_ID', 'TransID': 'ReceiptNo'}, inplace=True)
                     df = pd.merge(df1, df2[["TRANSFER_ID", "TransStatus", "ReceiptNo"]], on='TRANSFER_ID', how='inner')
                     print(df.head())
