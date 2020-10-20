d5053e3d7fd0b66308590fa0c0394b23ac241079
ezrankayamba <ezrankayamba@gmail.com>
2020-10-20 08:41:38 +0300

    Airtel path

--- utils.py
+++ utils.py
@@ -4,7 +4,9 @@ import fnmatch
 
 
 def get_file(path, tmpl, sftp):
+    print(path, "; ", tmpl)
     for file in sftp.listdir(path):
+        print(file)
         if fnmatch.fnmatch(file, tmpl):
             return file
     return tmpl
