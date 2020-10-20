75410d6ca03b727f54eacea387e82e3ece5d3ec5
ezrankayamba <ezrankayamba@gmail.com>
2020-10-20 07:57:02 +0300

    OUT DIR PATH

--- run.py
+++ run.py
@@ -24,17 +24,13 @@ with open('credentials.json') as creds_file:
                     # print(df1.head())
                     print(df2.head())
                     df2.rename(columns=cat['columns'], inplace=True)
-                    df = pd.merge(
-                        df1, df2[["TRANSFER_ID", "TransStatus", "ReceiptNo"]], on='TRANSFER_ID', how='left')
+                    df = pd.merge(df1, df2[["TRANSFER_ID", "TransStatus", "ReceiptNo"]], on='TRANSFER_ID', how='left')
                     print(df.head())
                     name = cat['name']
                     dt_str = tg_file_date.strftime('%Y%m%d')
                     path = f'outputs/{name}'
                     if not os.path.exists(path):
                         os.makedirs(path)
-                        df1.to_csv(
-                            f'{path}/Tigo_{name}_{dt_str}.csv', index=False)
-                        df2.to_csv(
-                            f'{path}/Other_{name}_{dt_str}.csv', index=False)
-                        df.to_csv(
-                            f'{path}/Result_{name}_{dt_str}.csv', index=False)
+                    df1.to_csv(f'{path}/Tigo_{name}_{dt_str}.csv', index=False)
+                    df2.to_csv(f'{path}/Other_{name}_{dt_str}.csv', index=False)
+                    df.to_csv(f'{path}/Result_{name}_{dt_str}.csv', index=False)
