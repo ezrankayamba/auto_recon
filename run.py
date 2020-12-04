a3ce3f9e77f1b49ae429cc70690a40c10278ddb0
ezrankayamba <ezrankayamba@gmail.com>
2020-12-04 16:45:12 +0300

    Bug fix

--- run.py
+++ run.py
@@ -14,36 +14,36 @@ with open('credentials.json') as creds_file:
     # print(creds)
     try:
         with pysftp.Connection(**creds) as sftp:
-        print('Connected...')
-        with open('config.json') as file:
-            categories = json.load(file)['categories']
+            print('Connected...')
+            with open('config.json') as file:
+                categories = json.load(file)['categories']
 
-            for cat in categories:
-                if not cat['enabled']:
-                    continue
-                try:
-                    r_f1, r_f2, tg_file_date, ot_file_date, sep, regex = utils.get_files(cat, sftp)
-                    print(r_f1, r_f2)
-                    with sftp.open(r_f1) as csv_file1, sftp.open(r_f2) as csv_file2:
-                        df1 = pd.read_csv(csv_file1)
-                        df2 = pd.read_csv(csv_file2, sep)
-                        # print(df1.head())
-                        print(df2.head())
-                        # df2.rename(columns=cat['columns'], inplace=True)
-                        if regex:
-                            df2.TRANSFER_ID.apply(lambda x: re.findall(regex, x)[0])
-                        df = pd.merge(df1, df2[["TRANSFER_ID", "TransStatus", "ReceiptNo"]], on='TRANSFER_ID', how='left')
-                        print(df.head())
-                        name = cat['name']
-                        dt_str = tg_file_date.strftime('%Y%m%d')
-                        path = f'outputs/{name}'
-                        if not os.path.exists(path):
-                            os.makedirs(path)
-                        df1.to_csv(f'{path}/Tigo_{name}_{dt_str}.csv', index=False)
-                        df2.to_csv(f'{path}/Other_{name}_{dt_str}.csv', index=False)
-                        df.to_csv(f'{path}/Result_{name}_{dt_str}.csv', index=False)
-                except Exception as ex:
-                    print(ex)
+                for cat in categories:
+                    if not cat['enabled']:
+                        continue
+                    try:
+                        r_f1, r_f2, tg_file_date, ot_file_date, sep, regex = utils.get_files(cat, sftp)
+                        print(r_f1, r_f2)
+                        with sftp.open(r_f1) as csv_file1, sftp.open(r_f2) as csv_file2:
+                            df1 = pd.read_csv(csv_file1)
+                            df2 = pd.read_csv(csv_file2, sep)
+                            # print(df1.head())
+                            print(df2.head())
+                            # df2.rename(columns=cat['columns'], inplace=True)
+                            if regex:
+                                df2.TRANSFER_ID.apply(lambda x: re.findall(regex, x)[0])
+                            df = pd.merge(df1, df2[["TRANSFER_ID", "TransStatus", "ReceiptNo"]], on='TRANSFER_ID', how='left')
+                            print(df.head())
+                            name = cat['name']
+                            dt_str = tg_file_date.strftime('%Y%m%d')
+                            path = f'outputs/{name}'
+                            if not os.path.exists(path):
+                                os.makedirs(path)
+                            df1.to_csv(f'{path}/Tigo_{name}_{dt_str}.csv', index=False)
+                            df2.to_csv(f'{path}/Other_{name}_{dt_str}.csv', index=False)
+                            df.to_csv(f'{path}/Result_{name}_{dt_str}.csv', index=False)
+                    except Exception as ex:
+                        print(ex)
 
     except Exception as ex:
         print(ex)
