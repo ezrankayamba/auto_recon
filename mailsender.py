4b5da3042cd2b0e3bb09ebf3d227547d194c3b9a
ezrankayamba <ezrankayamba@gmail.com>
2021-01-27 13:47:23 +0300

    Attach file

--- mailsender.py
+++ mailsender.py
@@ -12,6 +12,15 @@ config = configparser.ConfigParser()
 config.read('mailsender.ini')
 
 
+def attach_file(message, file_to_attach):
+    with open(file_to_attach, 'rb') as attach_file:
+        payload = MIMEBase('application', 'octate-stream')
+        payload.set_payload((attach_file).read())
+        encoders.encode_base64(payload)
+        payload.add_header('Content-Decomposition', 'attachment', filename=file_to_attach)
+        message.attach(payload)
+
+
 def send_mail():
     mail_content = 'Test Office 365'
     sender = config['DEFAULT']['SOURCE_ADDR']
@@ -22,6 +31,7 @@ def send_mail():
     message['To'] = to
     message['Subject'] = 'Test Mail'
     message.attach(MIMEText(mail_content, 'plain'))
+    attach_file(message, 'README.md')
     with SMTP(config['DEFAULT']['SMTP_SERVER'], config['DEFAULT']['PORT']) as smtp:
         smtp.noop()
         smtp.ehlo()
