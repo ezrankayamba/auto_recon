21f64433154914326bbbc610224bcf38d368f455
ezrankayamba <ezrankayamba@gmail.com>
2021-05-21 13:55:47 +0300

    Added error handling

--- bg.py
+++ bg.py
@@ -2,5 +2,5 @@ import threading
 
 
 def run_in_background(fn, args=None):
-    x = threading.Thread(target=fn, args=args)
+    x = threading.Thread(target=fn, args=args if args else ())
     x.start()
